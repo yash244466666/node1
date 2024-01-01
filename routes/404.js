7a85ef862b914a9c91faa268ad1d43a2c88a85ec
Yash <74345713+yash244466666@users.noreply.github.com>
2024-01-01 22:25:49 +0600

    Add routes files

---
+++ routes/404.js
@@ -0,0 +1,12 @@
+//404 page
+const path = require('path');
+const express = require('express');
+const router = express.Router(); 
+const rootDir = require('../util/path');
+
+
+router.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+module.exports = router;
