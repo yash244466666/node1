b46f5e8308813e3dad0bb7560b071b499f811713
Yash <74345713+yash244466666@users.noreply.github.com>
2024-01-02 19:46:15 +0600

    Add ejs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const notFoundRoutes = require('./routes/404');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const products = [];
 
 router.get('/add-product', (req, res, next) => {
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product');
+    res.render('add-product', {pageTitle: 'Add Products'});
 })
 
 router.post('/add-product', (req, res, next) => {
