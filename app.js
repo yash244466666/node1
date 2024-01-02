75655d4d88c9672456b511cf5384ab707567d556
Yash <74345713+yash244466666@users.noreply.github.com>
2024-01-02 11:04:02 +0600

    Add public path

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const notFoundRoutes = require('./routes/404'); 
+// const path = require('./util/path');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 
