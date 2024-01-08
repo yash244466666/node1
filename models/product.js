2ff5ca8542afb2f48f5594c5029d3234f04e1e39
Yash <74345713+yash244466666@users.noreply.github.com>
2024-01-08 12:47:16 +0600

    006 Loading Product Detail Data

--- models/product.js
+++ models/product.js
@@ -32,10 +32,19 @@ module.exports = class Products {
                 console.log(err);
             });
         });
-    }
+    };
 
     static fetchAll(cb) {
         getProductsFromFile(cb);
+    };
 
-    }
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+
+        });
+
+    };
+
 };
