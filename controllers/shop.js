5baf1cd728d579296f782e22d58045e0446c881f
Yash <74345713+yash244466666@users.noreply.github.com>
2024-01-13 11:37:22 +0600

    016 Displaying Cart Items on the Cart Page

--- controllers/shop.js
+++ controllers/shop.js
@@ -34,9 +34,21 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    res.render('shop/cart', {
-        docTitle: 'Your Cart',
-        currentRoute: '/cart'
+    Cart.getCart(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = [];
+            for (product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if(cartProductData) {
+                    cartProducts.push({productData: product, qty: cartProductData.qty})
+                }
+            }
+            res.render('shop/cart', {
+                docTitle: 'Your Cart',
+                currentRoute: '/cart',
+                products: cartProducts
+            });
+        })
     });
 };
 
